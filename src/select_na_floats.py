e5b69a4f8fbc41d321febb376239288bac9cc809
cgrdn <chris.gordon@dal.ca>
2021-08-12 11:15:05 -0300

    clean up index selection

--- src/select_na_floats.py
+++ src/select_na_floats.py
@@ -3,28 +3,22 @@
 import bgcArgoDMQC as bgc
 import argopy
 argopy.set_options(mode='expert')
+fetcher = argopy.IndexFetcher()
 
 # define "Northwest Atlantic" area
 na_box   = [-62, -36, 52, 65]
 
-# get index, subset to only floats with DOXY sensors
+na_index = fetcher.region(na_box).to_dataframe()
 bgc_index = bgc.get_index()
-bgc_index = bgc_index[~bgc_index.parameters.isna()]
-bgc_index = bgc_index[['DOXY' in p for p in bgc_index.parameters]]
-
-# subset index to only floats within our NW Atlantic box
-na_index  = bgc_index[bgc_index.longitude > na_box[0]]
-na_index  = na_index[na_index.longitude < na_box[1]]
-na_index  = na_index[na_index.latitude > na_box[2]]
-na_index  = na_index[na_index.latitude < na_box[3]]
-
+na_bgc_index = bgc_index[bgc_index.wmo.isin(na_index.wmo)]
+na_bgc_index = na_bgc_index[~na_bgc_index.parameters.isna()]
+na_doxy_index = na_bgc_index[['DOXY' in p for p in na_bgc_index.parameters]]
 # floats from after 2020 (as a starting point to minimize data load)
-# na_index  = na_index[na_index.date > 2.02e13]
+recent_index = na_doxy_index[na_doxy_index.date > 2.02e13]
 
 # argopy data fetcher
 fetcher = argopy.DataFetcher()
-ds = fetcher.float([int(w) for w in na_index.wmo.unique()]).to_xarray().to_dataframe()
+# ds = fetcher
 
 # for wmo in ds.PLATFORM_NUMBER.unique():
     # bgc.io.get_argo(wmo, local_path=bgc.ARGO_PATH)
-
